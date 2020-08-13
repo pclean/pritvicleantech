408c9ded9424ec6fa1e3033e8139fe3734754748
pclean <prithvicleantech@gmail.com>
2020-08-13 15:12:03 +0530

    Updates

---
+++ precache-manifest.14408b9c737af271d4ee1b911e751bcb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3942723c7834f1507030d912d4297268",
+    "url": "/pritvicleantech/index.html"
+  },
+  {
+    "revision": "a608ea8b48ffcf07afc3",
+    "url": "/pritvicleantech/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "bd56351f0550a95bf805",
+    "url": "/pritvicleantech/static/js/2.6e9dc9ce.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pritvicleantech/static/js/2.6e9dc9ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a608ea8b48ffcf07afc3",
+    "url": "/pritvicleantech/static/js/main.59ef35f4.chunk.js"
+  },
+  {
+    "revision": "fb350c90a42f8ac8ece6",
+    "url": "/pritvicleantech/static/js/runtime-main.3084f66f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pritvicleantech/static/media/logo.5d5d9eef.svg"
+  }
+]);
